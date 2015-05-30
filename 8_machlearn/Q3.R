1675377afe0704cf1462b1887253d8f1f440ce71
Fábio Franco Costa <fabio.costa@deltasigma.com.br>
2015-05-30 13:21:10 -0300

    Quiz 4 starting

--- 8_machlearn/Q3.R
+++ 8_machlearn/Q3.R
@@ -5,9 +5,16 @@ data(segmentationOriginal)
 library(caret)
 training <- segmentationOriginal[segmentationOriginal$Case == 'Train',]
 testing <- segmentationOriginal[segmentationOriginal$Case == 'Test',]
+
+# Remove columns not used
+drops <- c('Cell','Case')
+training <- training[,!(names(training) %in% drops)]
+
+
 set.seed(125)
 system.time({
-  CART <- train(Class ~ .,
+  # CART <- train(Class ~ .,
+  CART <- train(Class ~ TotalIntenCh2+FiberWidthCh1+PerimStatusCh1+VarIntenCh4,
                       data = training,
                       method=c("rpart"))
   })
@@ -16,18 +23,29 @@ system.time({
 predictions <- predict(CART, newdata = testing)
 confusionMatrix(predictions, testing$Class)
 
-TotalIntench2 = c(23000, 50000, 57000, NA)
+TotalIntenCh2 = c(23000, 50000, 57000, NA)
 FiberWidthCh1 = c(10, 10, 8, 8)
 PerimStatusCh1 = c(2, NA, NA, 2)
 VarIntenCh4 = c(NA, 100, 100, 100)
 
-quizData = data.frame(TotalIntench2,FiberWidthCh1,PerimStatusCh1,VarIntenCh4)
+quizData = data.frame(TotalIntenCh2,FiberWidthCh1,PerimStatusCh1,VarIntenCh4)
 
-quizData[0,]
+newData <- segmentationOriginal[-(1:2019),]
+newData[1:4,] <- rep(rep(NA,119),4)
 
-str(quizData[1,])
+for(i in 1:4) {
+  newData$TotalIntenCh2[i] <- quizData$TotalIntenCh2[i]
+  newData$FiberWidthCh1[i] <- quizData$FiberWidthCh1[i]
+  newData$PerimStatusCh1[i] <- quizData$PerimStatusCh1[i]
+  newData$VarIntenCh4[i] <- quizData$VarIntenCh4[i]
+}
 
-cases <- predict(CART, newdata = quizData[1,])
+# Remove columns not used
+newData <- newData[,!(names(newData) %in% drops)]
+
+predict(CART, newdata = newData[4,])
+
+cases <- predict(CART, newdata = newData)
 
 # Q4
 library(ElemStatLearn)
